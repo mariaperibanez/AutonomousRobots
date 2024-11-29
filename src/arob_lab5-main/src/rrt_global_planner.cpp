98f5e84ca6bf4d1dbb6c90241a3f9a2441d87c30
María Peribáñez Tafalla <817857@unizar.es>
2024-11-29 21:25:12 +0100

    Add files via upload
    practica5

---
+++ src/arob_lab5-main/src/rrt_global_planner.cpp
@@ -0,0 +1,232 @@
+#include <pluginlib/class_list_macros.h>
+#include "AROB_lab5/rrt_global_planner.h"
+
+//register this planner as a BaseGlobalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(rrt_planner::RRTPlanner, nav_core::BaseGlobalPlanner)
+
+//Default Constructor
+namespace rrt_planner {
+
+double distance(const unsigned int x0, const unsigned int y0, const unsigned int x1, const unsigned int y1){
+    return std::sqrt((int)(x1-x0)*(int)(x1-x0) + (int)(y1-y0)*(int)(y1-y0));
+}
+
+RRTPlanner::RRTPlanner() : costmap_ros_(NULL), initialized_(false),
+                            max_samples_(0.0){}
+
+RRTPlanner::RRTPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
+    initialize(name, costmap_ros);
+}
+
+void RRTPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
+
+    if (!initialized_){
+        ros::NodeHandle nh("~/" + name);
+        ros::NodeHandle nh_local("~/local_costmap/");
+        ros::NodeHandle nh_global("~/global_costmap/");
+
+        nh.param("maxsamples", max_samples_, 0.0);
+
+        //to make sure one of the nodes in the plan lies in the local costmap
+        double width, height;
+        nh_local.param("width", width, 3.0);
+        nh_local.param("height", height, 3.0);
+        max_dist_ = (std::min(width, height)/3.0);  //or any other distance within local costmap
+
+        nh_global.param("resolution", resolution_, 0.05);
+
+        // std::cout << "Parameters: " << max_samples_ << ", " << dist_th_ << ", " << visualize_markers_ << ", " << max_dist_ << std::endl;
+        // std::cout << "Local costmap size: " << width << ", " << height << std::endl;
+        // std::cout << "Global costmap resolution: " << resolution_ << std::endl;
+
+        costmap_ros_ = costmap_ros;
+        costmap_ = costmap_ros->getCostmap();
+        global_frame_id_ = costmap_ros_->getGlobalFrameID();
+
+        initialized_ = true;
+    }
+	else{
+	    ROS_WARN("This planner has already been initialized... doing nothing.");
+    }
+}
+
+bool RRTPlanner::makePlan(const geometry_msgs::PoseStamped& start, const geometry_msgs::PoseStamped& goal, 
+                            std::vector<geometry_msgs::PoseStamped>& plan ){
+
+    // std::cout << "RRTPlanner::makePlan" << std::endl;
+    
+    if (!initialized_){
+        ROS_ERROR("The planner has not been initialized.");
+        return false;
+    }
+
+	if (start.header.frame_id != costmap_ros_->getGlobalFrameID()){
+		ROS_ERROR("The start pose must be in the %s frame, but it is in the %s frame.",
+				  global_frame_id_.c_str(), start.header.frame_id.c_str());
+		return false;
+	}
+
+	if (goal.header.frame_id != costmap_ros_->getGlobalFrameID()){
+		ROS_ERROR("The goal pose must be in the %s frame, but it is in the %s frame.",
+				  global_frame_id_.c_str(), goal.header.frame_id.c_str());
+		return false;
+	}
+    
+    plan.clear();
+    costmap_ = costmap_ros_->getCostmap();  // Update information from costmap
+    
+    // Get start and goal poses in map coordinates
+    unsigned int goal_mx, goal_my, start_mx, start_my;
+    if (!costmap_->worldToMap(goal.pose.position.x, goal.pose.position.y, goal_mx, goal_my)){
+        ROS_WARN("Goal position is out of map bounds.");
+        return false;
+    }    
+    costmap_->worldToMap(start.pose.position.x, start.pose.position.y, start_mx, start_my);
+
+    std::vector<int> point_start{(int)start_mx,(int)start_my};
+    std::vector<int> point_goal{(int)goal_mx,(int)goal_my};    
+  	std::vector<std::vector<int>> solRRT;
+    bool computed = computeRRT(point_start, point_goal, solRRT);
+    if (computed){        
+        getPlan(solRRT, plan);
+        // add goal
+        plan.push_back(goal);
+    }else{
+        ROS_WARN("No plan computed");
+    }
+
+    return computed;
+}
+
+bool RRTPlanner::computeRRT(const std::vector<int> start, const std::vector<int> goal, 
+                            std::vector<std::vector<int>>& sol){
+    bool finished = false;
+    int iter = 0;
+
+    //Initialize random number generator
+    srand(time(NULL));
+        
+    // Initialize the tree with the starting point in map coordinates
+    TreeNode *itr_node = new TreeNode(start); 
+    std::vector<TreeNode*> tree;
+    tree.push_back(itr_node);
+
+    while ( iter < max_iter ){
+
+        iter++;
+
+        //Generate a random point inside the map
+        float x_rand = srand() % (costmap_->getSizeInCellsX() + 1); 
+        float y_rand = srand() % (costmap_->getSizeInCellsY() + 1); 
+        
+        // if punto del arbol y el goal estan sin obstaculos, acabar y devolver el true
+        if (!(costmap_->getCost(x_rand, y_rand) == costmap_2d::FREE_SPACE)){
+            continue;
+        }
+
+        std::vector <int> point{(int)x_rand, (int)y_rand};
+        TreeNode *new_node = new TreeNode(point);
+
+        //Find the nearest point in the tree
+        TreeNode *nearest = new_node->neast(itr_node);
+        unsigned int x0 = nearest->getNode()[0];
+        unsigned int y0 = nearest->getNode()[1];
+        // if maxdist actualizamos a la distancia maxima TODO: si nos returnea False, podemos hacer esto antes de la comprobacion de FREE_SPACE 
+         double dist = distance(x0, y0, x_rand, y_rand);
+        
+        if ( dist > max_dist_){
+            std::vector <double> direction{(double)(x0-x_rand)/dist, (double)(y1-y_rand)/dist}
+            x_rand = direction[0] * max_dist_;
+            y_rand = direction[1] * max_dist_;
+        }
+
+        // Verificar si el camino está libre de obstáculos
+        if(!obstacleFree( x0, y0, x_rand, y_rand )){
+            continue;
+        }
+
+        // Crear un nuevo nodo y agregarlo al árbol
+        TreeNode *new_node = new TreeNode({static_cast<float>(x_rand), static_cast<float>(y_rand)});
+        new_node->setParent(nearest);
+        tree.push_back(new_node);
+        
+        // Comprobar si se alcanza la meta
+        if (distance(x_rand, y_rand, goal[0], goal[1]) <= resolution_) {
+            // Reconstruir el camino desde la meta al inicio
+            TreeNode *current = new_node;
+            while (current != nullptr) {
+                sol.push_back(current->getNode());
+                current = current->getParent();
+            }
+            finished = true;
+            break;
+        }
+        
+    }
+
+    // implement RRT here!
+
+    itr_node->~TreeNode();
+
+    return finished;
+}
+
+bool RRTPlanner::obstacleFree(const unsigned int x0, const unsigned int y0, 
+                            const unsigned int x1, const unsigned int y1){
+    //Bresenham algorithm to check if the line between points (x0,y0) - (x1,y1) is free of collision
+
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    int incr_x = (dx > 0) ? 1.0 : -1.0;
+    int incr_y = (dy > 0) ? 1.0 : -1.0;
+
+    unsigned int da, db, incr_x_2, incr_y_2;
+    if (abs(dx) >= abs(dy)){
+        da = abs(dx); db = abs(dy);
+        incr_x_2 = incr_x; incr_y_2 = 0;
+    }else{
+        da = abs(dy); db = abs(dx);
+        incr_x_2 = 0; incr_y_2 = incr_y;
+    }
+
+    int p = 2*db - da;
+    unsigned int a = x0; 
+    unsigned int b = y0;
+    unsigned int end = da;
+    for (unsigned int i=0; i<end; i++){
+        if (costmap_->getCost(a, b) != costmap_2d::FREE_SPACE){  // to include cells with inflated cost
+            return false;
+        }else{
+            if (p >= 0){
+                a += incr_x;
+                b += incr_y;
+                p -= 2*da;
+            }else{
+                a += incr_x_2;
+                b += incr_y_2;
+            }
+            p += 2*db;
+        }
+    }
+
+    return true;
+}
+
+void RRTPlanner::getPlan(const std::vector<std::vector<int>> sol, std::vector<geometry_msgs::PoseStamped>& plan){
+
+    for (auto it = sol.rbegin(); it != sol.rend(); it++){
+        std::vector<int> point = (*it);
+        geometry_msgs::PoseStamped pose;
+
+        costmap_->mapToWorld((unsigned int)point[0], (unsigned int)point[1], 
+                            pose.pose.position.x, pose.pose.position.y);
+        pose.header.stamp = ros::Time::now();
+        pose.header.frame_id = global_frame_id_;
+        pose.pose.orientation.w = 1;
+        plan.push_back(pose);
+
+    }
+}
+
+};
